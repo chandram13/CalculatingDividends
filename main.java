187f6c7e7af86be91bd80177dae777c7e41d13c3
chandram13 <mschandr@uci.edu>
2022-08-08 21:11:10 -0700

    Added second formula

--- main.java
+++ main.java
@@ -2,7 +2,11 @@
 
 public class calcDividends {
     public static void payoutRatio{String [] dividends,netIncome){
-
         payout = dividends / netIncome
-
 }
+    public static void retentionRatio(String [] retainedEarnings, netIncome){
+    retainedRatio = retainedEarnings / netIncome
+        }
+
+
+
